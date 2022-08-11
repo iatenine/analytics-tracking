f588ffe1875f9884dd5385d4fffcb6635d435ecb
iatenine <iatenine@gmail.com>
2022-08-11 14:00:36 -0500

    Add cors stuff

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(routes);
 
